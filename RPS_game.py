1b0c07dfc20ca2adab3df7ced5c6a44265ef0f70
Aung Pyae Sone <101930261+Romulus004@users.noreply.github.com>
2024-02-23 12:33:16 +0700

    Update RPS_game.py

--- RPS_game.py
+++ RPS_game.py
@@ -40,15 +40,14 @@ x = Tk()
 x.title("RPS game")
 
 #Labels
-Label(x,text="Rock, Paper, Scissors",font=("Calibri",14)).grid(row=0,sticky=N,pady=10,padx=200)
-Label(x,text="Please select an option",font=("Calibri",12)).grid(row=1,sticky=N)
 player_score_label = Label(x,text="Player : 0",font=("Calibri",12))
 computer_score_label = Label(x,text="Computer : 0",font=("Calibri",12))
 player_choice_label = Label(x,font=("Calibri",12))
 computer_choice_label = Label(x,font=("Calibri",12))
 outcome_label = Label(x,font=("Calibri",12))
 
-player_score_label.grid(row=2,sticky=W)
+Label(x,text="Rock, Paper, Scissors",font=("Calibri",14)).grid(row=0,sticky=N,pady=10,padx=200)
+Label(x,text="Please select an option",font=("Calibri",12)).grid(row=1,sticky=N)player_score_label.grid(row=2,sticky=W)
 computer_score_label.grid(row=2,sticky=E)
 player_choice_label.grid(row=3,sticky=W)
 computer_choice_label.grid(row=3,sticky=E)
